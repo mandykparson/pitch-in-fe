0096c05810f4a76de7893ebc65db98082b22eb18
mandykparson <79546411+mandykparson@users.noreply.github.com>
2021-07-27 10:47:18 -0600

    posting pitches

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Homepage() {
+export default function Homepage(props) {
     return (
-        <div>
-            Homepage
+        <div className="container">
+            <h4>Welcome Back, {props.user.username}</h4>
+            <button>Add Bank</button>
         </div>
     )
 }
--- src/Components/Pitches.js
+++ src/Components/Pitches.js
@@ -1,19 +1,87 @@
 import React from 'react'
 import PitchCard from './PitchCard'
+import { useState } from 'react'
 
 
 export default function Pitches(props) {
 
+    const [ formPopUp, setFormPopUp ] = useState(false)
+    const [ title, setTitle ] = useState('')
+    const [ description, setDescription] = useState('')
+    const [ image, setImage ] = useState('')
+    const [ total, setTotal ] = useState(0)
+    const [ pitchesURL ] = useState('http://localhost:4000/pitches')
+
+    const togglePopUp = () => {
+        setFormPopUp(!formPopUp)
+    }
 
     const renderPitchCards = () => {
         return props.user.pitches.map(pitch => {
-            return <PitchCard pitch={pitch}/>
+            return <PitchCard key={pitch.title} pitch={pitch}/>
         })
     }
-    return (
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const newPitch = {
+            title: title,
+            description: description,
+            image: image,
+            total: total
+        }
+        fetch('http://localhost:4000/pitches', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'accept': "application/json",
+            },
+            body: JSON.stringify({ pitch: newPitch })
+        })
+    }
+
+    return (formPopUp) ? (
+        <div className="popup">
+            <div className="popup-inner">
+                <button className="close-btn" onClick={() => {setFormPopUp(!formPopUp)}}>X</button>
+                <h2>Create Pitch</h2>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Name Your Pitch"
+                        value={title}
+                        onChange={event => setTitle(event.target.value)}
+                    />
+                    <input
+                        type="text"
+                        name="description"
+                        placeholder="description"
+                        value={description}
+                        onChange={event => setDescription(event.target.value)}
+                    />
+                    <input
+                        type="text"
+                        name="image"
+                        placeholder="c + v your image url"
+                        value={image}
+                        onChange={event => setImage(event.target.value)}
+                    />
+                    <input
+                        type="number"
+                        name="total"
+                        placeholder="What you can contribute"
+                        value={total}
+                        onChange={event => setTotal(event.target.value)}
+                    />
+                    <input type="submit" value="Create"/>
+                </form>
+            </div>
+        </div>) 
+        : 
         <div className="container">
-            {renderPitchCards ()}
-            <button>Create Pitch</button>
-        </div>
-    )
+            {props.user.pitches ? renderPitchCards()
+            : "" }
+            <button onClick={togglePopUp}>Create Pitch</button> 
+        </div>;
 }
