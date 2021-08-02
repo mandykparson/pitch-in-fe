f021a55298c81a19e7f23a51a348258811a1cf9d
mandykparson <79546411+mandykparson@users.noreply.github.com>
2021-08-02 16:47:57 -0600

    adding post Request and buttons

---
+++ src/Components/Request.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+export default function Requests(props) {
+
+    const [ isDisabled, setIsDisabled ] = useState(false)
+
+    const handleClick = (oldTotal, deduction) => {
+        props.setNewFakeTot(oldTotal - deduction)
+        setIsDisabled(true)
+    }
+
+    return (
+        <div className="row">
+            <p className="seven columns">{props.request.description}</p>
+            <p className="two columns"><strong>${props.request.amount}.00</strong></p>
+            {isDisabled ? (
+                <p className="one column"><strong>PURCHASED</strong></p>
+            ) : (
+                <button disabled={isDisabled}className="three columns" onClick={() => handleClick(props.newFakeTot, props.request.amount)}>Buy</button>
+            )}
+        </div>
+    )
+}
